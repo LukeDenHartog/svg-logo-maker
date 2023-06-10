41513331dfe89fce1d8f95e4c9b171361209c1eb
Luke <121516961+Afrozez@users.noreply.github.com>
2023-06-09 21:26:07 -0700

    created some user input options

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 let inquirer = require('inquirer');
 // const jest = require('jest');
 const fs = require('fs');
-
-inquirer
-  .prompt([
+const { title } = require('process');
+inquirer.prompt([
     /* Pass your questions in here */
    {
     type: 'input',
-    name: 'text-input',
-    message: 'What text would you like to add to the logo? (Max 3 characters)'
+    name: 'textInput',
+    message: 'What three characters would you like to add to the logo?'
+  },
+  {
+    type: 'input',
+    name: 'textColorInput',
+    message: 'What color would you like the text inside the logo to be?'
+  },
+  {
+    type: 'list',
+    name: 'shapeInput',
+    message: 'What text would you like to add to the logo? (Max 3 characters)',
+    choices: ['Circle', 'Triangle', 'Square']
+  },
+  {
+    type: 'input',
+    name: 'shapeColorInput',
+    message: 'What color would you like the logo to be?'
   }
 ])
-.then()
+.then((answers) => {
+ logoText = answers.textInput;
+ logoTextColor = answers.textColor;
+ logoShape = answers.shapeInput;
+ logoColor = answers.shapeColorInput;
+console.log(logoColor, logoShape, logoText, logoTextColor);
+
+ 
+
+/*fs.writeFile('logo.svg', completedLogo, (err) => {
+  if (err) {
+    console.error('Error writing file:', err);
+    return;
+  }
+  console.log('Logo created successfully.');
+});*/
+console.log('')
+});
+
