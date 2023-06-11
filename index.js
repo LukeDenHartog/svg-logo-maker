524842964010bb6d8b48ed87081ece5bec7b69fc
Luke <121516961+Afrozez@users.noreply.github.com>
2023-06-11 09:38:10 -0700

    added template literal

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let inquirer = require('inquirer');
 // const jest = require('jest');
 const fs = require('fs');
 const { title } = require('process');
+const {Shape, Circle, Triangle, Square} = require('./lib/shapes');
+
 inquirer.prompt([
     /* Pass your questions in here */
    {
@@ -27,21 +29,45 @@ inquirer.prompt([
   }
 ])
 .then((answers) => {
- logoText = answers.textInput;
- logoTextColor = answers.textColor;
- logoShape = answers.shapeInput;
- logoColor = answers.shapeColorInput;
-console.log(logoColor, logoShape, logoText, logoTextColor);
+ let logoText = answers.textInput;
+ let textColor = answers.textColorInput;
+ answers.shapeInput; // this is the shape the user has selected.
+ let logoColor = answers.shapeColorInput;
+console.log(logoColor, logoText, textColor);
+
+if (answers.shapeInput === 'Circle') {
+  logoShape = new Circle().render();
+}
+
+
+console.log(logoShape);
+
+svgElement = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+\n\n
+<text x="150" y="125" text-anchor="middle" fill="${textColor}">${logoText}</text>
+\n\n
+
+</svg>
+\n\n
+
+`;
+
+
 
  
+// let completedLogo = `${}`
 
-/*fs.writeFile('logo.svg', completedLogo, (err) => {
+ fs.writeFile('logo.svg', svgElement, (err) => {
   if (err) {
     console.error('Error writing file:', err);
     return;
   }
   console.log('Logo created successfully.');
-});*/
-console.log('')
 });
+
+
+
+});
+
+
 
